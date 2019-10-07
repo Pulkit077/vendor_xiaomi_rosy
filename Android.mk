ca0f0eaa7f95ddcebdcee09ba7e84cc5b5254973
Stallix <team.evolutionx.rom@gmail.com>
2019-10-07 04:38:36 +0000

    msm8953-common: Update IMS from Raphael

--- Android.mk
+++ Android.mk
@@ -171,7 +171,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := ims
 LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/priv-app/ims/ims.apk
+LOCAL_SRC_FILES := proprietary/product/priv-app/ims/ims.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
