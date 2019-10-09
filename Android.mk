cad253c6f49f48e4b742f28ba2ef50faa9c0b2ce
dianlujitao <dianlujitao@lineageos.org>
2019-10-09 12:16:35 +0800

    rosy: Update Alarm blobs from LA.UM.8.6.r1-01900-89xx.0
    Change-Id: I5db2d80e8701b85e836480cc0ca99a6fa742f1ab

--- Android.mk
+++ Android.mk
@@ -195,13 +195,13 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := PowerOffAlarm
 LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/vendor/app/PowerOffAlarm/PowerOffAlarm.apk
+LOCAL_SRC_FILES := proprietary/product/app/PowerOffAlarm/PowerOffAlarm.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := .apk
-LOCAL_VENDOR_MODULE := true
+LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
