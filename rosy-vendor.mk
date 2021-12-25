6023166255439594897d39aecc63543e9f5ec562
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-12-25 20:59:25 +0530

    rosy: don't dex vendor.qti.hardware.fingerprint-V1.0-java
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- rosy-vendor.mk
+++ rosy-vendor.mk
@@ -7,6 +7,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/rosy/proprietary/etc/permissions/com.qti.snapdragon.sdk.display.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.qti.snapdragon.sdk.display.xml \
+    vendor/xiaomi/rosy/proprietary/vendor/framework/vendor.qti.hardware.fingerprint-V1.0-java.jar:$(TARGET_COPY_OUT_VENDOR)/framework/vendor.qti.hardware.fingerprint-V1.0-java.jar \
     vendor/xiaomi/rosy/proprietary/vendor/lib/android.hardware.biometrics.fingerprint@2.1.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.biometrics.fingerprint@2.1.so \
     vendor/xiaomi/rosy/proprietary/lib/libFileMux.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libFileMux.so \
     vendor/xiaomi/rosy/proprietary/lib/libOmxMux.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libOmxMux.so \
@@ -1491,7 +1492,6 @@ PRODUCT_PACKAGES += \
     qcrilmsgtunnel \
     com.qti.snapdragon.sdk.display \
     tcmclient \
-    vendor.qti.hardware.fingerprint-V1.0-java \
     com.fingerprints.extension \
     com.qti.dpmframework \
     com.qualcomm.qti.imscmservice-V2.0-java \
